b018d01e252e6af80945b6ebbe3674684eeea9e8
colmprunty <colmpruntysignups+github@gmail.com>
2015-02-22 18:30:42 +0000

    setup stuff

---
+++ HouseBills/Controllers/DebugController.cs
@@ -0,0 +1,34 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using HouseBills.Entities;
+using HouseBills.Entities.Maps;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace HouseBills.Controllers
+{
+    [TestFixture]
+    public class DebugController : ControllerBase
+    {
+        [Test]
+        public void CreateDatabase()
+        {
+            var cfg = Fluently.Configure()
+                .Database(MsSqlConfiguration
+                    .MsSql2008
+                    .ConnectionString(c => c.FromConnectionStringWithKey("OfflineConnectionString")))
+                .Mappings(m => m.FluentMappings
+                    .AddFromAssemblyOf<TenantMap>());
+
+            cfg.ExposeConfiguration(x => new SchemaExport(x).Execute(true, true, false))
+                .BuildConfiguration();
+        }
+
+        [Test]
+        public void AddSomeone()
+        {
+            var newbie = new Tenant("Colm");
+            NhSession.Save(newbie);
+        }
+    }
+}
